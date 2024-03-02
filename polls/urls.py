e1762a6c47bcf3ebd0f6ff63e32eb6d9c8c89225
Omkar Amlan Krishna <omkaramlankrishna@gmail.com>
2024-03-02 10:57:52 +0530

    upload page

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from polls import views
 
 urlpatterns = [
     path('handle_csv_upload/', views.handle_csv_upload, name='handle_csv_upload'),
+    path('handle_dropdown/', views.handle_dropdown, name='handle_dropdown'),
     path('admin/', admin.site.urls),
     path('', views.index, name='index')
 ]
--- polls/views.py
+++ polls/views.py
@@ -11,12 +11,29 @@ def index(request):
 def handle_csv_upload(request):
     if request.method == 'POST' and request.FILES['csvFile']:
         csv_file = request.FILES['csvFile']
-
-        if not csv_file.name.endswith('.csv'):
-            return HttpResponse('Invalid file. Please upload a CSV file.')
-
         csv_data = csv_file.read().decode('utf-8')
         df = pd.read_csv(io.StringIO(csv_data))
-        return render(request,'upload.html')
+        df_limited = df.head(100)
+        context={
+            'df': df,
+            'df_limited': df_limited,
+        }
+        request.session['df'] = df.to_json(orient='records')
+        request.session['df_limited'] = df_limited.to_json(orient='records')
+        return render(request,'upload.html',context=context)
     else:
         return HttpResponse('Invalid request.')
+
+def handle_dropdown(request):
+    df_json = request.session.get('df')
+    df_limited_json = request.session.get('df_limited')
+    df = pd.read_json(df_json)
+    df_limited = pd.read_json(df_limited_json)
+    if request.method == 'POST':
+        selected_column=request.POST.get('selectedColumn', '')
+        context={
+            'df': df,
+            'df_limited': df_limited,
+            'selected_column':selected_column,
+        }
+        return render(request,'get_column.html',context=context)
